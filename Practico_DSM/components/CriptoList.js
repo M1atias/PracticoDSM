4f33512a6f6316639ed1cd8b7d7601803413a24b
LucasGabrielActisAgudiak <lucasactis99@gmail.com>
2021-06-24 19:59:23 -0300

    [CriptoList] Fix and clean

--- Practico_DSM/components/CriptoList.js
+++ Practico_DSM/components/CriptoList.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 import {Text, View,FlatList,StyleSheet,TextInput} from 'react-native';
 import CriptoDetail from './CriptoDetail';
 
@@ -7,30 +6,15 @@ const CriptoList = (props) => {
     const [coins, setCoins] = useState(null)
     const [search, setSearch] = useState('')
     const [refreshing,setRefreshing] = useState(false)
-    const [data,setData] = useState(null)
-    const [coinLoad, setCoinLoad] = useState(false)
     
     const loadData = async () => {
         const res = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false')
         setCoins(await res.json())
     }
     useEffect(()=>{
-        // Revisar esto con axios
-        /*axios.get(
-            'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false'
-        ).then((res)=>{
-            setCoins(await res.json())
-        })*/
         loadData()
         
     },[])
-
-    const buscarCoin = () => {
-        coinData = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))
-        setData(coinData)
-        setCoins(coinData)
-    }
-
 
     if(!coins){
         return(
@@ -40,26 +24,28 @@ const CriptoList = (props) => {
         )
     }
     
-    //Ver si en el FlatList el data se lo puede poner como funcion y retornar un Loading si no se encuentran datos
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <TextInput placeholder="Search coin" placeholderTextColor='#858585' style={styles.searchInput} onChangeText={text=>setSearch(text)}/>
             </View>
-          <FlatList style={styles.list} data={coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))}
-          renderItem={({item}) => {
-              return <CriptoDetail coin={item}/>
-          }} 
-          showsVerticalScrollIndicator={false}
-          refreshing={refreshing}
-          onRefresh={async()=>{
-              setRefreshing(true)
-              await loadData()
-              setRefreshing(false)
-          }}
-          />
+            <FlatList style={styles.list} data={coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase()))}
+                renderItem={({item}) => { return <CriptoDetail coin={item}/> }} 
+                showsVerticalScrollIndicator={false}
+                refreshing={refreshing}
+                onRefresh={async()=>{
+                    setRefreshing(true)
+                    await loadData()
+                    setRefreshing(false)
+                }}
+                ListEmptyComponent={
+                    <View style={styles.container}>
+                        <Text>No matches for the search.</Text>
+                    </View>
+                }
+            />
         </View>
-      ); 
+    ); 
 }
 
 const styles = StyleSheet.create({
